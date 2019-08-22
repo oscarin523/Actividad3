a074fdb88e411265e501c6aa54d31af1b4c587d7
oscar eduardo melo <omelo@ucol.mx>
2019-08-21 21:13:49 -0500

    Correxccion Código C#

---
+++ Actividad 3 - Ciclos For (Oscar)/Actividad 3 - Ciclos For (Oscar)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Actividad_3___Ciclos_For__Oscar_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Imprime los numeros de x hasta y de -1");
+            Console.WriteLine("De donde arrancamos el ciclo (Mayor)");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De donde terminamos el ciclo (Menor)");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De cuanto en cuanto");
+            z = Convert.ToInt32(Console.ReadLine());
+
+           if (x > y)
+            {
+                for (int i = x; i > y; i -= z)
+                {
+                    Console.WriteLine("i= " + i);
+                }
+            }
+            else
+            {
+                for (int i = x; i < y; i+=z)
+                {
+                    Console.WriteLine("i= " + i);
+                }
+            }
+        }
+     }
+}
